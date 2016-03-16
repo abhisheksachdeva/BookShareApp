d8a8742f9b189644cba4f72a86d182d59ea50398
abhisheksachdeva <sachdeva351@gmail.com>
2016-03-17 03:51:16 +0530

    Changed to Goodreads API

--- app/src/main/java/com/example/abhishek/bookshareapp/api/BooksAPI.java
+++ app/src/main/java/com/example/abhishek/bookshareapp/api/BooksAPI.java
@@ -4,7 +4,7 @@ package com.example.abhishek.bookshareapp.api;
  * Created by abhishek on 30/1/16.
  */
 
-import com.example.abhishek.bookshareapp.api.models.BookResponse;
+import com.example.abhishek.bookshareapp.api.models.GoodreadsResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -13,9 +13,10 @@ import retrofit2.http.Query;
 
 public interface BooksAPI {
 
-    @GET("books/v1/volumes")
-    Call<BookResponse> getBooks(
-            @Query("q") String isbn
+    @GET("search.xml")
+    Call<GoodreadsResponse> getBooks(
+        @Query("q") String keyword,
+        @Query("key") String key
     );
 
 }
