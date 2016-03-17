bf238955b36600c5df1c643d6b4bd46cf1248b9e
abhisheksachdeva <sachdeva351@gmail.com>
2016-03-17 22:51:42 +0530

    Added customAdapter

--- app/src/main/java/com/example/abhishek/bookshareapp/ui/SearchResultsActivity.java
+++ app/src/main/java/com/example/abhishek/bookshareapp/ui/SearchResultsActivity.java
@@ -14,6 +14,7 @@ import com.example.abhishek.bookshareapp.api.BooksAPI;
 import com.example.abhishek.bookshareapp.api.models.Book;
 import com.example.abhishek.bookshareapp.api.models.GoodreadsResponse;
 import com.example.abhishek.bookshareapp.api.models.Search;
+import com.example.abhishek.bookshareapp.ui.adapter.BooksAdapter;
 import com.example.abhishek.bookshareapp.utils.CommonUtilities;
 
 import java.util.List;
@@ -29,6 +30,7 @@ public class SearchResultsActivity extends AppCompatActivity {
     String id;
     String query;
     List<Book> bookList;
+    BooksAdapter adapter;
     ListView resultsList;
     String API_KEY= CommonUtilities.API_KEY;
     Search sr;
@@ -56,8 +58,13 @@ public class SearchResultsActivity extends AppCompatActivity {
         call.enqueue(new Callback<GoodreadsResponse>() {
             @Override
             public void onResponse(Call<GoodreadsResponse> call, Response<GoodreadsResponse> response) {
-                Log.d("searchresp","searchOnresp");
+                Log.d("searchresp",response.toString());
                 sr= response.body().getSearch();
+                bookList= sr.getBooks();
+                Log.d("searchresp",bookList.toString());
+                adapter= new BooksAdapter(SearchResultsActivity.this,bookList);
+                resultsList.setAdapter(adapter);
+
                 Toast.makeText(SearchResultsActivity.this,sr.getBooks().get(0).getBookDetails().getAuthor().getAuthor_name(),Toast.LENGTH_SHORT).show();
 
             }
--- app/src/main/java/com/example/abhishek/bookshareapp/ui/adapter/BooksAdapter.java
+++ app/src/main/java/com/example/abhishek/bookshareapp/ui/adapter/BooksAdapter.java
@@ -1,116 +1,119 @@
-//package com.example.abhishek.bookshareapp.ui.adapter;
-//
-//import android.content.Context;
-//import android.util.Log;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.BaseAdapter;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import com.example.abhishek.bookshareapp.R;
-//import com.example.abhishek.bookshareapp.api.models.Book;
-//import com.example.abhishek.bookshareapp.ui.SearchResultsActivity;
-//import com.squareup.picasso.Picasso;
-//
-//import java.util.List;
-//
-///**
-// * Created by abhishek on 30/1/16.
-// */
-//public class BooksAdapter extends BaseAdapter implements View.OnClickListener {
-//
-//    private Context context;
-//    private List<Book> bookList;
-//    private static LayoutInflater inflater=null;
-//    Book tempValues=null;
-//
-//    public BooksAdapter(Context context,List<Book> bookList){
-//        this.bookList =bookList;
-//        this.context=context;
-//        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        Log.d("BookAdapter","Constructor");
-//    }
-//
-//    @Override
-//    public int getCount() {
-//
-//        return bookList.size();
-//    }
-//
-//    @Override
-//    public Object getItem(int position) {
-//        return bookList.get(position);
-//    }
-//
-//    @Override
-//    public long getItemId(int position) {
-//        return bookList.get(position).getISBN();
-//    }
-//
-//    public static class ViewHolder{
-//        public TextView title_book;
-//        public TextView author_book;
-//        public ImageView image_book;
-//    }
-//
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        Log.d("BookAdapter","getView");
-//        View vi = convertView;
-//        ViewHolder holder;
-//        if(convertView == null){
-//            vi = inflater.inflate(R.layout.row_books,null);
-//
-//            holder = new ViewHolder();
-//            holder.author_book = (TextView)vi.findViewById(R.id.row_books_author);
-//            holder.title_book = (TextView)vi.findViewById(R.id.row_books_title);
-//            holder.image_book = (ImageView)vi.findViewById(R.id.row_books_imageView);
-//
-//            vi.setTag(holder);
-//        }
-//        else{
-//            holder=(ViewHolder)vi.getTag();
-//        }
-//        if(bookList.size()<=0) {
-//            holder.title_book.setText("No Data");
-//        }
-//        else{
-//            tempValues=null;
-//            tempValues=bookList.get(position);
-//
-//            holder.title_book.setText(tempValues.getVolumeInfo().getTitle());
-//            holder.author_book.setText(tempValues.getVolumeInfo().getAllAuthors());
-//            Picasso.with(this.context).load(tempValues.getVolumeInfo().getImageLinks().getSmallThumbnail()).into(holder.image_book);
-//
-//            vi.setOnClickListener(new OnItemClickListener(position));
-//        }
-//
-//        return vi;
-//    }
-//
-//    @Override
-//    public void onClick(View v) {
-//        Log.v("CustomAdapter", "=====Row button clicked=====");
-//    }
-//
-//    private class OnItemClickListener  implements View.OnClickListener {
-//        private int mPosition;
-//
-//        OnItemClickListener(int position){
-//            mPosition = position;
-//        }
-//
-//        @Override
-//        public void onClick(View arg0) {
-//
-//
-//            SearchResultsActivity sct = (SearchResultsActivity)context;
-//
-//            /****  Call  onItemClick Method inside CustomListViewAndroidExample Class ( See Below )****/
-//
-//            sct.onItemClick(mPosition);
-//        }
-//    }
-//}
+package com.example.abhishek.bookshareapp.ui.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.abhishek.bookshareapp.R;
+import com.example.abhishek.bookshareapp.api.models.Book;
+import com.example.abhishek.bookshareapp.ui.SearchResultsActivity;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by abhishek on 30/1/16.
+ */
+public class BooksAdapter extends BaseAdapter implements View.OnClickListener {
+
+    private Context context;
+    private List<Book> bookList;
+    private static LayoutInflater inflater=null;
+    Book tempValues=null;
+
+    public BooksAdapter(Context context,List<Book> bookList){
+        this.bookList =bookList;
+        this.context=context;
+        inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        Log.d("BookAdapter","Constructor");
+    }
+
+
+
+    @Override
+    public Object getItem(int position) {
+        return bookList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public static class ViewHolder{
+        public TextView title_book;
+        public TextView author_book;
+        public ImageView image_book;
+    }
+
+    @Override
+    public int getCount() {
+        return bookList.size();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Log.d("BookAdapter","getView");
+        View vi = convertView;
+        ViewHolder holder;
+        if(convertView == null){
+            vi = inflater.inflate(R.layout.row_books,null);
+
+            holder = new ViewHolder();
+            holder.author_book = (TextView)vi.findViewById(R.id.row_books_author);
+            holder.title_book = (TextView)vi.findViewById(R.id.row_books_title);
+            holder.image_book = (ImageView)vi.findViewById(R.id.row_books_imageView);
+
+            vi.setTag(holder);
+        }
+        else{
+            holder=(ViewHolder)vi.getTag();
+        }
+        if(bookList.size()<=0) {
+            holder.title_book.setText("No Data");
+        }
+        else{
+            tempValues=null;
+            tempValues=bookList.get(position);
+
+            holder.title_book.setText(tempValues.getBookDetails().getTitle());
+            holder.author_book.setText(tempValues.getBookDetails().getAuthor().getAuthor_name());
+            Picasso.with(this.context).load(tempValues.getBookDetails().getImage_url()).into(holder.image_book);
+
+            vi.setOnClickListener(new OnItemClickListener(position));
+        }
+
+        return vi;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.v("CustomAdapter", "=====Row button clicked=====");
+    }
+
+    private class OnItemClickListener  implements View.OnClickListener {
+        private int mPosition;
+
+        OnItemClickListener(int position){
+            mPosition = position;
+        }
+
+        @Override
+        public void onClick(View arg0) {
+
+
+            SearchResultsActivity sct = (SearchResultsActivity)context;
+
+            /****  Call  onItemClick Method inside CustomListViewAndroidExample Class ( See Below )****/
+
+           // sct.onItemClick(mPosition);
+            Toast.makeText(context,"Clicked",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
