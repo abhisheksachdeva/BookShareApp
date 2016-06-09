456b3c14caf28200653db60c80be051562b02043
Ajayrahul P <ajayrahul97@gmail.com>
2016-06-09 17:35:41 +0530

    Added Notification icon

---
+++ app/src/main/java/com/example/abhishek/bookshareapp/ui/Notifications.java
@@ -0,0 +1,13 @@
+package com.example.abhishek.bookshareapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Notifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+    }
+}
