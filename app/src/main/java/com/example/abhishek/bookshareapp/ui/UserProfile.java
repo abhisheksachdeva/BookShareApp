834398b44b3bdb2936dd2d2876d3fffe06c0af39
Abhishek Sachdeva <sachdeva351@gmail.com>
2016-07-17 06:51:59 +0530

    Changed UserProfile

--- app/src/main/java/com/example/abhishek/bookshareapp/ui/UserProfile.java
+++ app/src/main/java/com/example/abhishek/bookshareapp/ui/UserProfile.java
@@ -1,6 +1,7 @@
 package com.example.abhishek.bookshareapp.ui;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -10,8 +11,11 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.abhishek.bookshareapp.R;
 import com.example.abhishek.bookshareapp.api.NetworkingFactory;
@@ -25,36 +29,29 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 
+import jp.wasabeef.blurry.Blurry;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class UserProfile extends AppCompatActivity {
-    TextView userName,userEmail,address;
+    TextView name,email,address, booksCount;
     UserInfo user;
     List<Book> booksList;
     BooksAdapterSimple adapter;
-    CollapsingToolbarLayout collapsingToolbarLayout;
-    ImageView image;
+    ImageView profile_picture, background_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        userName = (TextView)findViewById(R.id.username);
-        userEmail = (TextView)findViewById(R.id.useremail);
+        name = (TextView)findViewById(R.id.name);
+        email = (TextView)findViewById(R.id.email);
         address = (TextView)findViewById(R.id.address);
-
+        profile_picture = (ImageView) findViewById(R.id.profile_picture);
+        background_image = (ImageView) findViewById(R.id.background_image);
+        booksCount = (TextView) findViewById(R.id.books_count);
         String id = getIntent().getExtras().getString("id");
-
-        image = (ImageView) findViewById(R.id.img);
-        image.setImageDrawable(getResources().getDrawable(R.drawable.books));
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar1));
-        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        collapsingToolbarLayout.setTitle("User Profile");
-        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
-        setPalette();
-
 
         RecyclerView userBooksList = (RecyclerView) findViewById(R.id.userBooksLists);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
@@ -83,13 +80,22 @@ public class UserProfile extends AppCompatActivity {
                 if(response.body()!=null) {
                     Log.d("UserProfile Response:", response.toString());
                     user = response.body();
-                    userName.setText( user.getFirstName() + " "+user.getLastName());
-                    userEmail.setText( user.getEmail());
-                    collapsingToolbarLayout.setTitle(user.getFirstName() + " "+user.getLastName());
-                    address.setText( user.getRoomNo()+", "+user.getHostel());
+                    name.setText(user.getName());
+                    email.setText(user.getEmail());
+                    String ad = user.getRoomNo()+", "+user.getHostel();
+                    address.setText(ad);
                     String url = CommonUtilities.local_books_api_url + "image/"+id+"/";
-                    Picasso.with(UserProfile.this).load(url).into(image);
+                    Picasso.with(UserProfile.this).load(url).into(profile_picture);
+                    Picasso.with(UserProfile.this).load(url).into(background_image);
+                    Blurry.with(UserProfile.this)
+                            .radius(40)
+                            .sampling(1)
+                            .color(Color.argb(66, 0, 0, 0))
+                            .async()
+                            .capture(findViewById(R.id.background_image))
+                            .into((ImageView) findViewById(R.id.background_image));
                     List<Book> booksTempInfoList = user.getUserBookList();
+                    booksCount.setText(booksTempInfoList.size()+"");
                     booksList.clear();
                     booksList.addAll(booksTempInfoList);
                     adapter.notifyDataSetChanged();
@@ -107,19 +113,6 @@ public class UserProfile extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         finish();
-    }
-
-    private void setPalette() {
-        Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
-        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-            @Override
-            public void onGenerated(Palette palette) {
-                int primaryDark = getResources().getColor(R.color.colorPrimaryDark);
-                int primary = getResources().getColor(R.color.colorPrimary);
-                collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
-                collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkVibrantColor(primaryDark));
-            }
-        });
     }
 
 }
