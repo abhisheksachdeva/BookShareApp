7a7f5e8d4d49dd8270d6ca9deb6c9df2dc2a2103
iamharsshit <harshit.bansalec@gmail.com>
2017-08-16 22:53:57 +0530

    confirm password activity view modified

--- app/src/main/java/com/sdsmdg/bookshareapp/BSA/ui/PasswordConfirmActivity.java
+++ app/src/main/java/com/sdsmdg/bookshareapp/BSA/ui/PasswordConfirmActivity.java
@@ -1,9 +1,11 @@
 package com.sdsmdg.bookshareapp.BSA.ui;
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Handler;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -11,6 +13,7 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sdsmdg.bookshareapp.BSA.R;
@@ -24,6 +27,7 @@ import retrofit2.Response;
 
 public class PasswordConfirmActivity extends AppCompatActivity {
 
+    private TextView helperTextView;
     private TextInputLayout newPasswordInputLayout, confirmPasswordInputLayout;
     private TextInputEditText newPasswordEditText, confirmPasswordEditText;
     private Button submitButton;
@@ -45,30 +49,10 @@ public class PasswordConfirmActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 String password = newPasswordEditText.getText().toString();
                 if (TextUtils.isEmpty(password) || password.length() < 8){
-                    newPasswordInputLayout.setErrorEnabled(true);
-                    newPasswordInputLayout.setError("The password must atleast consist of 8 characters.");
+                    helperTextView.setTextColor(Color.RED);
                 } else{
-                    newPasswordInputLayout.setErrorEnabled(false);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-            }
-        });
-
-        confirmPasswordEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if (newPasswordEditText.getText().toString().equals(confirmPasswordEditText.getText().toString())){
-                    confirmPasswordInputLayout.setErrorEnabled(false);
-                } else{
-                    confirmPasswordInputLayout.setErrorEnabled(true);
-                    confirmPasswordInputLayout.setError("");
+                    helperTextView.setTextColor(ContextCompat.getColor
+                            (PasswordConfirmActivity.this, R.color.colorAccent));
                 }
             }
 
@@ -80,10 +64,15 @@ public class PasswordConfirmActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!newPasswordEditText.getText().toString().equals(confirmPasswordEditText.getText().toString())
-                        || newPasswordEditText.getText().length() < 8){
+                if (newPasswordEditText.getText().length() < 8){
+                    newPasswordInputLayout.setErrorEnabled(true);
+                    confirmPasswordInputLayout.setErrorEnabled(false);
+                    newPasswordInputLayout.setError("The password must be of atleast 8 characters");
+                } else if(!newPasswordEditText.getText().toString().equals(confirmPasswordEditText.getText().toString())){
+                    newPasswordInputLayout.setErrorEnabled(false);
+                    confirmPasswordInputLayout.setErrorEnabled(true);
                     confirmPasswordInputLayout.setError("The passwords do not match");
-                } else{
+                } else {
                     newPasswordInputLayout.setEnabled(false);
                     confirmPasswordInputLayout.setEnabled(false);
                     resetPassword();
@@ -124,6 +113,7 @@ public class PasswordConfirmActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+        helperTextView = (TextView) findViewById(R.id.txt_reset_password_message);
         newPasswordInputLayout = (TextInputLayout) findViewById(R.id.new_password_edit_text_layout);
         confirmPasswordInputLayout = (TextInputLayout) findViewById(R.id.cnf_password_edit_text_layout);
         newPasswordEditText =  (TextInputEditText) findViewById(R.id.new_pwd_edit_text);
