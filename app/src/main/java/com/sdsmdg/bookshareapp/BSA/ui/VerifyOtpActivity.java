bfcfb71f3f3101a1ad11f474b1373145b5369ae7
iamharsshit <harshit.bansalec@gmail.com>
2017-10-04 16:21:49 +0530

    make changes for requests and auto read otp

--- app/src/main/java/com/sdsmdg/bookshareapp/BSA/ui/VerifyOtpActivity.java
+++ app/src/main/java/com/sdsmdg/bookshareapp/BSA/ui/VerifyOtpActivity.java
@@ -1,10 +1,18 @@
 package com.sdsmdg.bookshareapp.BSA.ui;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.app.Service;
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,12 +21,28 @@ import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.sdsmdg.bookshareapp.BSA.Listeners.SmsListener;
+import com.sdsmdg.bookshareapp.BSA.Listeners.SmsReceiver;
+import com.sdsmdg.bookshareapp.BSA.Manifest;
 import com.sdsmdg.bookshareapp.BSA.R;
+import com.sdsmdg.bookshareapp.BSA.api.NetworkingFactory;
+import com.sdsmdg.bookshareapp.BSA.api.UsersAPI;
+import com.sdsmdg.bookshareapp.BSA.api.models.Signup;
+import com.sdsmdg.bookshareapp.BSA.api.models.VerifyToken.Detail;
 
-public class VerifyOtpActivity extends AppCompatActivity implements View.OnFocusChangeListener, TextWatcher, View.OnKeyListener {
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+public class VerifyOtpActivity extends AppCompatActivity implements
+        View.OnFocusChangeListener, TextWatcher, SmsListener {
+
+    private static final  int MY_PERMISSIONS_REQUEST_READ_SMS = 100;
     private TextInputEditText pinFirstDigitEditText;
     private TextInputEditText pinSecondDigitEditText;
     private TextInputEditText pinThirdDigitEditText;
@@ -26,6 +50,9 @@ public class VerifyOtpActivity extends AppCompatActivity implements View.OnFocus
     private TextInputEditText pinFifthDigitEditText;
     private TextInputEditText pinSixthDigitEditText;
     private TextInputEditText pinHiddenEditText;
+    private Button submitOtpButton;
+    private CustomProgressDialog progressDialog;
+    private String email = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +60,148 @@ public class VerifyOtpActivity extends AppCompatActivity implements View.OnFocus
         setContentView(R.layout.activity_verify_otp);
         initViews();
         setPinListeners();
+        checkSmsPermission();
+        bindListeners();
+        regListeners();
+        if (getIntent().getExtras() != null){
+            email = (String) getIntent().getExtras().get("email");
+        }
+    }
+
+    /**
+     * Register listeners for button click
+     */
+    private void regListeners() {
+        submitOtpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (pinHiddenEditText.getText().length() == 6){
+                    submitOtpButton.setEnabled(false);
+                    progressDialog = new CustomProgressDialog(VerifyOtpActivity.this);
+                    progressDialog.setCancelable(false);
+                    progressDialog.show();
+                    final UsersAPI usersAPI = NetworkingFactory.getLocalInstance().getUsersAPI();
+                    Call<Detail> verifyOtpCall = usersAPI.
+                            verifyOtp(email, pinHiddenEditText.getText().toString());
+                    verifyOtpCall.enqueue(new Callback<Detail>() {
+                        @Override
+                        public void onResponse(Call<Detail> call, Response<Detail> response) {
+                            if (response.body() != null){
+                                String detail = response.body().getDetail();
+                                if (detail.equals("Activation Link Sent")) {
+                                    onSignupSuccess();
+                                } else {
+                                    onSignupFailed("The otp entered is incorrect.");
+                                    progressDialog.dismiss();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Detail> call, Throwable t) {
+                            onSignupFailed("Check your network connection properly");
+                            progressDialog.dismiss();
+                        }
+                    });
+                } else{
+                    Toast.makeText(VerifyOtpActivity.this,
+                            "The otp must be atleast 6 numbers in length", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void onSignupSuccess() {
+        submitOtpButton.setEnabled(true);
+        progressDialog.dismiss();
+        setResult(RESULT_OK, null);
+        showAlertDialog();
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Success!!");
+        builder.setMessage("An activation link has been sent to your email. Click it to activate your citadel account.");
+        builder.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+                startActivity(new Intent(VerifyOtpActivity.this, LoginActivity.class));
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.setCancelable(false);
+        dialog.show();
+    }
+
+    public void onSignupFailed(String toast) {
+        Toast.makeText(getBaseContext(), toast, Toast.LENGTH_LONG).show();
+        submitOtpButton.setEnabled(true);
+    }
+
+    /**
+     * Check permission for reading sms
+     */
+    private void checkSmsPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.READ_SMS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Read Sms Permission");
+                builder.setMessage("Allow Citadel app to read your sms to automatically verify one time password sent.");
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.READ_SMS},
+                        MY_PERMISSIONS_REQUEST_READ_SMS);
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_READ_SMS:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    PackageManager pm  = this.getPackageManager();
+                    ComponentName componentName = new ComponentName(this, SmsReceiver.class);
+                    pm.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                            PackageManager.DONT_KILL_APP);
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    PackageManager pm  = this.getPackageManager();
+                    ComponentName componentName = new ComponentName(this, SmsReceiver.class);
+                    pm.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                            PackageManager.DONT_KILL_APP);
+                }
+                break;
+        }
+    }
+
+    /**
+     * Bind to receiver to receive message
+     */
+    private void bindListeners() {
+        SmsReceiver.bindListener(this);
     }
 
     /**
@@ -46,6 +215,7 @@ public class VerifyOtpActivity extends AppCompatActivity implements View.OnFocus
         pinFifthDigitEditText = (TextInputEditText) findViewById(R.id.txt_fifth_pin);
         pinSixthDigitEditText = (TextInputEditText) findViewById(R.id.txt_sixth_pin);
         pinHiddenEditText = (TextInputEditText) findViewById(R.id.pin_hidden_edittext);
+        submitOtpButton = (Button) findViewById(R.id.submit_otp_button);
     }
 
     /**
@@ -60,14 +230,6 @@ public class VerifyOtpActivity extends AppCompatActivity implements View.OnFocus
         pinFourthDigitEditText.setOnFocusChangeListener(this);
         pinFifthDigitEditText.setOnFocusChangeListener(this);
         pinSixthDigitEditText.setOnFocusChangeListener(this);
-
-        pinFirstDigitEditText.setOnKeyListener(this);
-        pinSecondDigitEditText.setOnKeyListener(this);
-        pinThirdDigitEditText.setOnKeyListener(this);
-        pinFourthDigitEditText.setOnKeyListener(this);
-        pinFifthDigitEditText.setOnKeyListener(this);
-        pinSixthDigitEditText.setOnKeyListener(this);
-        pinHiddenEditText.setOnKeyListener(this);
     }
 
     @Override
@@ -117,6 +279,8 @@ public class VerifyOtpActivity extends AppCompatActivity implements View.OnFocus
     public static void setFocus(EditText editText) {
         if (editText == null)
             return;
+        editText.setFocusable(true);
+        editText.setFocusableInTouchMode(true);
         editText.requestFocus();
     }
 
@@ -157,6 +321,11 @@ public class VerifyOtpActivity extends AppCompatActivity implements View.OnFocus
         if (s.length() == 0) {
             setFocusedPinBackground(pinFirstDigitEditText);
             pinFirstDigitEditText.setText("");
+            pinSecondDigitEditText.setText("");
+            pinThirdDigitEditText.setText("");
+            pinFourthDigitEditText.setText("");
+            pinFifthDigitEditText.setText("");
+            pinSixthDigitEditText.setText("");
         } else if (s.length() == 1) {
             setFocusedPinBackground(pinSecondDigitEditText);
             pinFirstDigitEditText.setText(s.charAt(0) + "");
@@ -198,35 +367,16 @@ public class VerifyOtpActivity extends AppCompatActivity implements View.OnFocus
     }
 
     @Override
-    public boolean onKey(View view, int keyCode, KeyEvent event) {
-        if (event.getAction() == KeyEvent.ACTION_DOWN) {
-            final int id = view.getId();
-            switch (id) {
-                case R.id.pin_hidden_edittext:
-                    if (keyCode == KeyEvent.KEYCODE_DEL) {
-                        if (pinHiddenEditText.getText().length() == 6)
-                            pinSixthDigitEditText.setText("");
-                        else if (pinHiddenEditText.getText().length() == 5)
-                            pinFifthDigitEditText.setText("");
-                        else if (pinHiddenEditText.getText().length() == 4)
-                            pinFourthDigitEditText.setText("");
-                        else if (pinHiddenEditText.getText().length() == 3)
-                            pinThirdDigitEditText.setText("");
-                        else if (pinHiddenEditText.getText().length() == 2)
-                            pinSecondDigitEditText.setText("");
-                        else if (pinHiddenEditText.getText().length() == 1)
-                            pinFirstDigitEditText.setText("");
-
-                        if (pinHiddenEditText.length() > 0)
-                            pinHiddenEditText.setText(pinHiddenEditText.getText().subSequence(0, pinHiddenEditText.length() - 1));
-
-                        return true;
-                    }
-                    break;
-                default:
-                    return false;
-            }
-        }
-        return false;
+    public void messageReceived(String messageText) {
+        String[] words = messageText.split(" ");
+        String otp = words[8];
+        pinHiddenEditText.setText(otp);
+        pinHiddenEditText.setSelection(pinHiddenEditText.getText().length());
+        pinFirstDigitEditText.setText(otp.charAt(0) + "");
+        pinSecondDigitEditText.setText(otp.charAt(1) + "");
+        pinThirdDigitEditText.setText(otp.charAt(2) + "");
+        pinFourthDigitEditText.setText(otp.charAt(3) + "");
+        pinFifthDigitEditText.setText(otp.charAt(4) + "");
+        pinSixthDigitEditText.setText(otp.charAt(5) + "");
     }
 }
